670cb103856743e34d78215da9a667df3120e173
lxh15 <1392649188@qq.com>
2022-07-06 19:28:41 +0800

    表单案例完成 ，待优化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>表格案例</h1>
+    <MyTable></MyTable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 引入表格
+import MyTable from './viwes/MyTable.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MyTable,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/viwes/MyTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <UseTable>
+      <template #uname="scope">
+        <input type="text" name="" id="" v-model="scope.row.name" />
+      </template>
+      <template #age="scope">
+        <input type="text" name="" id="" v-model="scope.row.age" />
+      </template>
+      <template #img="scope">
+        <!-- <a href=""></a>
+        <img src="" alt="" /> -->
+        {{ arr[scope.row.type] }}
+      </template>
+    </UseTable>
+  </div>
+</template>
+
+<script>
+// 引入
+import UseTable from '../components/input/UseTable.vue';
+export default {
+  data() {
+    return {
+      arr: [
+        { a: `<a :href="scope.row.headImgUrl"></a>` },
+        { img: ` <img :src="scope.row.headImgUrl" alt="">` },
+        { img: ` <img :src="scope.row.headImgUrl" alt="">` },
+        {
+          a: ` <input type="text" name="" id="" v-model="scope.row.headImgUrl" />`,
+        },
+      ],
+    };
+  },
+  components: {
+    UseTable,
+  },
+};
+</script>
+
+<style></style>

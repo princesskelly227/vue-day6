112bdb107b31afc0079adfc61cd0650f0e4302a5
kelly227800 <2529303733@qq.com>
2022-07-07 00:43:13 +0800

    练习了表单渲染作业,新建了UseTable.vue保存App.vue代码

---
+++ src/components/MyTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+      <table border="1" rules="all" cellpadding='15'>
+          <thead>
+              <tr>
+                  <th>序号</th>
+                  <th>姓名</th>
+                  <th>年龄</th>
+                  <th>头像</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="(item,index) in arr" :key="index">
+                  <td>
+                    <slot name="index">
+                        {{index+1}}
+                    </slot>
+                  </td>
+                  <td>
+                    <slot name="name">
+                        {{item.name}}
+                    </slot>
+                  </td>
+                  <td>
+                    <slot name="age">
+                        {{item.age}}
+                    </slot>
+                  </td>
+                  <td>
+                    <slot name="type" :row="item">
+                        {{item.headImgUrl}}
+                    </slot>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        arr: {
+            type: Array,
+            default: () => {[]}
+        }
+    }
+
+}
+</script>
+<style>
+table{
+    margin: 0 auto;
+}
+tbody{
+    text-align: center;
+}
+    
+</style>

fd2c9380fe7b873bd4f28c902373abc6df35d347
kelly227800 <2529303733@qq.com>
2022-07-06 23:50:23 +0800

    练习了手机邮箱tab切换，新建了 Register.vue保存App.vue

---
+++ src/components/EmailRegister.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+        <p>邮箱账号</p>
+        <input type="text" name="" id="" class="one" placeholder="输入邮箱账号"><br>
+        <input type="text" name="" id="" class="one text" placeholder="输入验证码">
+        <button>发送验证码</button>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PhoneRegister.vue
@@ -0,0 +1,17 @@
+<template>
+    <form>
+        <p>手机号码</p>
+        <input type="text" name="" id="" class="one" placeholder="输入手机号码"><br>
+        <input type="text" name="" id="" class="one text" placeholder="输入验证码">
+        <button>发送验证码</button>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
